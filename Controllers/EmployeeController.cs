4f5472ae768601978c9b8994b9c14a5f0d2fd4a6
Lucas <Lucas@DESKTOP-8PD740H>
2024-01-29 20:21:26 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Model;
+using WebApi.ViewModel;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/V1/employee")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository ?? throw new ArgumentNullException();
+        }
+
+        [HttpPost]
+        public IActionResult Add(EmployeeViewModel employeeView)
+        {
+            var emplyee = new Employee(employeeView.Name, employeeView.age, null);
+
+            _employeeRepository.Add(emplyee);
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var employees = _employeeRepository.Get();
+            return Ok(employees);
+        }
+    }
+}
---
+++ Infra/EmployeeRepository.cs
@@ -0,0 +1,20 @@
+﻿using WebApi.Model;
+
+namespace WebApi.Infra
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly ConnectionContext _Context = new();
+
+        public void Add(Employee employee)
+        {
+            _Context.Employees.Add(employee);
+            _Context.SaveChanges();
+        }
+
+        public List<Employee> Get()
+        {
+            return _Context.Employees.ToList();
+        }
+    }
+}
